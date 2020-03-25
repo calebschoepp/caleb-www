41e14e3cce0f78b9e5a16a6b560a5d1d95aacaf0
Caleb <caleb.schoepp@gmail.com>
2020-03-24 23:12:22 -0700

    Pick font color, link color, and font face

--- themes/caleb-www-theme/assets/css/tailwind.config.js
+++ themes/caleb-www-theme/assets/css/tailwind.config.js
@@ -2,10 +2,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        opensans: ["Open-sans", "sans-serif"],
-        lato: ["Lato", "sans-serif"],
-        zillaslab: ["Zilla-slab", "sans-serif"],
-        ubuntu: ["Ubuntu", "sans-serif"]
+        lato: ["Lato", "sans-serif"]
       }
     }
   },
